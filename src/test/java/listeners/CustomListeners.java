962f7b43eae8249ea8b9b8d7bb111b9ed7d243e6
nagakowsalya <nagakowsalya@gmail.com>
2021-10-11 00:50:22 +0100

    third commit

--- src/test/java/listeners/CustomListeners.java
+++ src/test/java/listeners/CustomListeners.java
@@ -1,6 +1,14 @@
 package listeners;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -13,11 +21,13 @@ import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 
 import base.TestBase;
+import utilities.MonitoringMail;
+import utilities.TestConfig;
 import utilities.TestUtil;
 
-public class CustomListeners extends TestBase implements ITestListener{
+public class CustomListeners extends TestBase implements ITestListener,ISuiteListener{
 	
-	
+	static String messageBody;
 
 	public void onTestStart(ITestResult result) {
 	
@@ -82,6 +92,35 @@ public class CustomListeners extends TestBase implements ITestListener{
 	}
 
 	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+public void onFinish(ISuite arg0) {
+		
+		MonitoringMail mail = new MonitoringMail();
+		 
+		try {
+			messageBody = "http://" + InetAddress.getLocalHost().getHostAddress()
+					+ ":8080/job/DataDrivenLiveProject/Extent_Reports/";
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		try {
+			mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+	}
+
+	public void onStart(ISuite arg0) {
 		// TODO Auto-generated method stub
 		
 	}
